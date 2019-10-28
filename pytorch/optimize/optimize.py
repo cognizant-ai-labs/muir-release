3530d7ed8314b4f7a5fb4aca7cfecc288b04c2a6
ekmeyerson <elliot.meyerson@cognizant.com>
2019-10-27 20:40:54 -0700

    update license path for opt code

--- pytorch/optimize/optimize.py
+++ pytorch/optimize/optimize.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2019 Cognizant Digital Business.
-# Issued under this Academic Public License: github.com/leaf-ai/muir/LICENSE.
+# Issued under this Academic Public License: github.com/leaf-ai/muir/pytorch/optimize/LICENSE.
 #
 # Entrypoint for optimization in muir
 #
